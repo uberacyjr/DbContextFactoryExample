e8c58d2718df30e2c282364cff401aafbbc2e344
Uberacy Tottoli <uberacyjr@gmail.com>
2022-03-06 10:53:19 -0300

    Add reference link

--- ConsoleApp2/MyContextFactory.cs
+++ ConsoleApp2/MyContextFactory.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace DbContextFactoryExample.Console
 {
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli#from-a-design-time-factory
+    /// </summary>
     public class MyContextFactory : IDesignTimeDbContextFactory<MyContext>
     {
         public MyContext CreateDbContext(string[] args)
