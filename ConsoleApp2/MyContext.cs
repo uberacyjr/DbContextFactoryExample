a0093412433a35f8396ac6b250f677d152ae66eb
Uberacy Tottoli <uberacyjr@gmail.com>
2022-03-06 10:33:06 -0300

    Add project files.

---
+++ ConsoleApp2/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using DbContextFactoryExample.Console.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbContextFactoryExample.Console
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ConsoleApp2/MyContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DbContextFactoryExample.Console
+{
+    public class MyContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseSqlServer("server=localhost;database=MyDatabase;trusted_connection=true");
+
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
